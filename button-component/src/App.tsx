ccc6ef3733fcfb70d21b476d6289180006be1581
Shalaka More <42513520+shalakashelly@users.noreply.github.com>
2024-11-17 16:52:23 -0500

    Install React, TypeScript and Tailwind CSS and get the local started.

---
+++ button-component/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <div className="mt-6 flex items-center justify-center gap-x-6">
+          <button type="button" className="text-sm/6 font-semibold text-gray-900">
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Save
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ button-component/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust to match your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
